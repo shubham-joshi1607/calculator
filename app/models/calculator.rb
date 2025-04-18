ce8fc531e4116e0af6fd644f06a7bc374f4f1864
shubham-joshi1607 <shubh.joshi96@gmail.com>
2025-04-18 19:39:53 +0530

    Step 3: Handle sum of two comma-separated numbers

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,12 +1,9 @@
 class Calculator
-    def add(numbers)
-      0
-    end
 
     def add(numbers)
         return 0 if numbers.empty?
-        numbers.to_i
+        numbers.split(',').map(&:to_i).sum
     end
-      
+ 
 end
   
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,7 +8,12 @@ RSpec.describe Calculator do
     end
 
     it 'returns the number itself when one number is provided' do
-        expect(Calculator.new.add("1")).to eq(1)
-    end      
+      expect(Calculator.new.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two numbers' do
+      expect(Calculator.new.add("1,2")).to eq(3)
+    end
+      
   end
 end
